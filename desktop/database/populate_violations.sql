1cc9e110c403a7d023e60a343b0ec71576ea4c66
saulcho009 <saulcho009@gmail.com>
2016-04-20 15:44:14 -0400

    Added data for violations table

---
+++ desktop/database/populate_violations.sql
@@ -0,0 +1,5 @@
+INSERT INTO violation VALUES (0, "C", "3-401.11", "Raw Animal Foods", "F", "");
+INSERT INTO violation VALUES (1, "N", "3-601.12", "Honestly Presented", "F", "");
+INSERT INTO violation VALUES (2, "C", "590.006A", "Water, Plumbing Waste", "S", "");
+INSERT INTO violation VALUES (3, "N", "590.009A", "Caterers", "S", "");
+INSERT INTO violation VALUES (4, "C", "Local 001", "No Coals", "L", "");
