fe6bf2aaa494b2a816eabdc73d7439629300278e
Jacob Downs <jdowns@umass.edu>
2016-04-20 15:46:02 -0400

    first day work, templated landing page

---
+++ desktop/src/Dashboard.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Created by Jdown_000 on 4/13/2016.
+ */
+public class Dashboard {
+    private JPanel panel1;
+    private JButton logoutButton;
+    private JButton syncButton;
+    private JButton searchInspectionsButton;
+}
