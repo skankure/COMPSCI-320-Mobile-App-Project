e505f28b371104bd53dae5627be84fe67b0b7c09
Andrew Raleigh <andrewsraleigh@gmail.com>
2016-04-24 18:19:14 -0400

    Adding db data access object

---
+++ desktop/database/sqliteconnection.java
@@ -0,0 +1,16 @@
+import java.sql.*;
+
+public class sqliteconnection {
+
+    Connection conn = null;
+
+    public static Connection dbConnect(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:/Users/Andrew/Desktop/HIMA/CS320/desktop/database/database.db");
+            return conn;
+        } catch {
+            System.out.println("Error connecting to SQLite Database!");
+        }
+    }
+}
