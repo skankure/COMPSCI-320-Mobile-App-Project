c28e60955838990bb60c077fe52486d05336e9c2
Andrew Raleigh <andrewsraleigh@gmail.com>
2016-04-25 12:57:24 -0400

    Fixing return statement

--- desktop/database/sqliteconnection.java
+++ desktop/database/sqliteconnection.java
@@ -5,12 +5,14 @@ public class sqliteconnection {
     Connection conn = null;
 
     public static Connection dbConnect(){
+
         try {
             Class.forName("org.sqlite.JDBC");
             Connection conn = DriverManager.getConnection("jdbc:sqlite:/Users/Andrew/Desktop/HIMA/CS320/desktop/database/database.db");
             return conn;
         } catch {
             System.out.println("Error connecting to SQLite Database!");
+            return null;
         }
     }
 }
