f2d7bb963121a339bca1829a732900c64f1ecb6d
dianetam523 <diane.tam523@gmail.com>
2016-05-01 19:09:02 -0400

    Final android build

--- android/app/src/main/java/com/pandi/hima/Perform3.java
+++ android/app/src/main/java/com/pandi/hima/Perform3.java
@@ -1,11 +1,19 @@
 package com.pandi.hima;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class Perform3 extends AppCompatActivity {
 
@@ -15,15 +23,37 @@ public class Perform3 extends AppCompatActivity {
         setContentView(R.layout.activity_perform3);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+    public void buttonPreview(View v) {
+        Button button =(Button) v;
+        displayFinalPDF();
+    }
+
+    public void buttonPrint(View v) {
+        Button button =(Button) v;
+        startActivity(new Intent(getApplicationContext(), Print.class));
+    }
+
+    public void displayFinalPDF() {
+
+        File file = null;
+        file = new File(Environment.getExternalStorageDirectory()+ "/final.pdf");
+        Toast.makeText(getApplicationContext(), file.toString(), Toast.LENGTH_LONG).show();
+        if(file.exists()) {
+            Intent target = new Intent(Intent.ACTION_VIEW);
+            target.setDataAndType(Uri.fromFile(file), "application/pdf");
+            target.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+
+            Intent intent = Intent.createChooser(target, "Open File");
+            try {
+                startActivity(intent);
+            } catch (ActivityNotFoundException e) {
+                // Instruct the user to install a PDF reader here, or something
             }
-        });
+        }
+        else
+            Toast.makeText(getApplicationContext(), "File path is incorrect." , Toast.LENGTH_LONG).show();
     }
 
 }
